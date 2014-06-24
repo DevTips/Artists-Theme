0b55603d60ac450a00ddb9fbc08794097e0c92c8
Ariel Safari <arielsafari@gmail.com>
2014-06-24 21:32:20 +0300

    Add smootheScrool function
    As a nice bonus to adding anchors to the top and buttom menu, heres a function that going to make a smooth scroll at the page from anchor to anchor.

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,3 +1,18 @@
 $(function() {
-  
+	smoothScrool(1000);
 });
+
+// smoothScroll function is applied from the document ready function
+function smoothScrool (duration) {
+	$('a[href^="#"]').on('click', function(event) {
+
+	    var target = $( $(this).attr('href') );
+
+	    if( target.length ) {
+	        event.preventDefault();
+	        $('html, body').animate({
+	            scrollTop: target.offset().top
+	        }, duration);
+	    }
+	});
+}
