d29ca29c4e3ee10492ae836e23ef2ebe550335c7
Riccardo Pasianotto <riccardo.pasianotto@gmail.com>
2014-07-13 17:30:58 +0200

    Added task in Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -26,14 +26,43 @@ module.exports = function(grunt) {
           config: '_config.yml'
         }
       }
+    },
+    watch: {
+      settings: {
+        files: ['_data/*.yml'],
+        tasks: ['jeky-build']
+      },
+      sass: {
+        files: ['assets/_sass/**/*.sass','assets/_sass/**/*.scss'],
+        tasks: ['sass']
+      },
+      script: {
+        files: ['assets/js/*.js'],
+        tasks: ['jeky-build']
+      }
+    },
+    connect: {
+      server: {
+        options: {
+          port: 9000,
+          base: '_site'
+        }
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+
   grunt.loadNpmTasks('grunt-file-append');
   grunt.loadNpmTasks('grunt-jekyll');
 
   // Default task(s).
   grunt.registerTask('default', ['sass','file_append','jekyll']);
+  grunt.registerTask('sass', ['sass','file_append','jekyll']);
+  grunt.registerTask('jeky-build', ['jekyll']);
+  grunt.registerTask('build', ['sass','file_append','jekyll']);
+  grunt.registerTask('serve', ['connect','watch']);
 
 };
