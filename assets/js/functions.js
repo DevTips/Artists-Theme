0e0666d8ff6bbaedd8949e3f584e4c1032fed433
travisneilson <travisneilson@gmail.com>
2014-08-17 15:18:29 -0700

    completing clients section

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -2,6 +2,9 @@ $(function() {
 	smoothScroll(300);
 	workBelt();
 	workLoad();
+	
+	clientStuff();
+	
 });
 
 // smoothScroll function is applied from the document ready function
@@ -53,5 +56,62 @@ function  workLoad() {
   });
   
 }
+
+
+
+
+function clientStuff() {
+  
+  $('.client-unit').first().addClass('active-client');
+  $('.client-logo').first().addClass('active-client');
+  
+  $('.client-logo').click(function() {
+    var $this = $(this),
+        $siblings = $this.parent().children(),
+        position = $siblings.index($this);
+        
+    $('.client-unit').removeClass('active-client').eq(position).addClass('active-client');
+    $siblings.removeClass('active-client');
+    $this.addClass('active-client');
+  });
+  
+  
+  $('.client-control-next, .client-control-prev').click(function() {
+  
+    var $this = $(this),
+        curActiveClient = $('.clients-belt').find('.active-client'),
+        position = $('.clients-belt').children().index(curActiveClient),
+        clientNum = $('.client-unit').length;
+        
+      if($this.hasClass('client-control-next')) {
+        
+        if(position < clientNum -1){
+          $('.active-client').removeClass('active-client').next().addClass('active-client');
+        } else {
+          $('.client-unit').removeClass('active-client').first().addClass('active-client');
+          $('.client-logo').removeClass('active-client').first().addClass('active-client');
+        }
+        
+      } else {
+        
+        if (position === 0) {
+          $('.client-unit').removeClass('active-client').last().addClass('active-client');
+          $('.client-logo').removeClass('active-client').last().addClass('active-client');
+        } else {
+          $('.active-client').removeClass('active-client').prev().addClass('active-client');  
+        }
+
+      }
+        
+  
+  });
+  
+}
+
+
+
+
+
+
 
 
