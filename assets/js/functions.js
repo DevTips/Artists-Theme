35c872abccd74c43c8d07b70b9043eb805869652
travisneilson <travisneilson@gmail.com>
2014-09-01 20:45:38 -0700

    Made the header responsive using "fitText"

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -5,6 +5,8 @@ $(function() {
 	
 	clientStuff();
 	
+	$("header h1").fitText(1, { minFontSize: '20px', maxFontSize: '72px' })
+	
 });
 
 // smoothScroll function is applied from the document ready function
@@ -112,7 +114,38 @@ function clientStuff() {
 }
 
 
+(function( $ ){
 
+  $.fn.fitText = function( kompressor, options ) {
+
+    // Setup options
+    var compressor = kompressor || 1,
+        settings = $.extend({
+          'minFontSize' : Number.NEGATIVE_INFINITY,
+          'maxFontSize' : Number.POSITIVE_INFINITY
+        }, options);
+
+    return this.each(function(){
+
+      // Store the object
+      var $this = $(this);
+
+      // Resizer() resizes items based on the object width divided by the compressor * 10
+      var resizer = function () {
+        $this.css('font-size', Math.max(Math.min($this.width() / (compressor*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
+      };
+
+      // Call once to set.
+      resizer();
+
+      // Call on resize. Opera debounces their resize by default.
+      $(window).on('resize.fittext orientationchange.fittext', resizer);
+
+    });
+
+  };
+
+})( jQuery );
 
 
 
